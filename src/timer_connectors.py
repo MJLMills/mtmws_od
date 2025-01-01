c47118b840fbfe9e99405247f945138e73eb1d0d
mjlmills <mjohnmills@gmail.com>
2024-12-31 22:44:14 -0600

    connect all output hardware and looper to the timer interrupt

--- src/timer_connectors.py
+++ src/timer_connectors.py
@@ -13,44 +13,57 @@ class TimerConnector:
 class WriteOutput(TimerConnector):
     """Action to take whenever the output timer runs."""
     def __init__(self,
-                 lorenz_system_a,
-                 lorenz_system_b,
+                 looper,
                  cv_output_x_a,
                  cv_output_z_a,
                  cv_output_x_b,
                  cv_output_z_b,
+                 pulse_output_a,
+                 pulse_output_b,
+                 pulse_led_a,
+                 pulse_led_b,
                  freq):
 
-        super().__init__(inputs=[lorenz_system_a,
-                                 lorenz_system_b],
-                         outputs=[cv_output_x_a,
+        super().__init__(inputs=[looper],
+                         outputs=[looper,
+                                  cv_output_x_a,
                                   cv_output_z_a,
                                   cv_output_x_b,
-                                  cv_output_z_b],
+                                  cv_output_z_b,
+                                  pulse_output_a,
+                                  pulse_output_b,
+                                  pulse_led_a,
+                                  pulse_led_b],
                          freq=freq)
 
-        self._lorenz_system_a = lorenz_system_a
-        self._lorenz_system_b = lorenz_system_b
+        self._looper = looper
 
         self._cv_output_x_a = cv_output_x_a
         self._cv_output_z_a = cv_output_z_a
         self._cv_output_x_b = cv_output_x_b
         self._cv_output_z_b = cv_output_z_b
 
-    def callback(self, _):
-        """Write the outputs"""
-        self._cv_output_z_a.write(self._lorenz_system_a.value_z_u16)
-        self._cv_output_x_a.write(self._lorenz_system_a.value_x_u16)
-        self._cv_output_z_b.write(self._lorenz_system_b.value_z_u16)
-        self._cv_output_x_b.write(self._lorenz_system_b.value_x_u16)
+        self._pulse_output_a = pulse_output_a
+        self._pulse_output_b = pulse_output_b
+        self._pulse_led_a = pulse_led_a
+        self._pulse_led_b = pulse_led_b
 
-        # TODO - hook this up
-        # if self._lorenz_system_a.crossed_zero:
-        #     led_matrix.turn_on(led_id_pulse_output_a)
-        #     pulse_output_a.pulse(led_pulse_output_length)
-        #     led_matrix.turn_off(led_id_pulse_output_a)
-        #
-        # if self._lorenz_system_b.crossed_zero:
-        #     led_matrix.turn_on(led_id_pulse_output_b)
-        #     pulse_output_b.pulse(led_pulse_output_length)
-        #     led_matrix.turn_off(led_id_pulse_output_b)
+    def callback(self, _):
+        """Write the properly scaled outputs."""
+
+        self._cv_output_z_a.write(self._looper.lorenz_system_a.mapped_z_value)
+        self._cv_output_x_a.write(self._looper.lorenz_system_a.mapped_x_value)
+        self._cv_output_z_b.write(self._looper.lorenz_system_b.mapped_z_value)
+        self._cv_output_x_b.write(self._looper.lorenz_system_b.mapped_x_value)
+
+        if self._looper.lorenz_system_a.crossed_zero:
+            self._pulse_led_a.turn_on()
+            self._pulse_output_a.pulse(0.01)
+            self._pulse_led_a.turn_off()
+
+        if self._looper.lorenz_system_b.crossed_zero:
+            self._pulse_led_b.turn_on()
+            self._pulse_output_a.pulse(0.01)
+            self._pulse_led_b.turn_off()
+
+        self._looper.needs_update = True

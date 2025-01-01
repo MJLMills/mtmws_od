27b65f33603e1eb69788358262534cc2c6f2e0f4
mjlmills <mjohnmills@gmail.com>
2024-12-31 23:39:39 -0600

    create a ranged value class

---
+++ src/ranged_value.py
@@ -0,0 +1,19 @@
+class RangedValue:
+    def __init__(self, min_value, max_value):
+        self._value = None
+        self._min_value = min_value
+        self._max_value = max_value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    def min_value(self):
+        return self._min_value
+
+    def max_value(self):
+        return self._max_value

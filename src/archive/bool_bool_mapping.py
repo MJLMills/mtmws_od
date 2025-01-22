5fd9c1df2f66b9caea71745e578add7f9d60f091
mjlmills <mjohnmills@gmail.com>
2025-01-21 20:44:28 -0600

    remove archived code

--- src/archive/bool_bool_mapping.py
+++
@@ -1,8 +0,0 @@
-# class BoolBoolMapping(object):
-#
-#     def __init__(self, signal, slot):
-#         self._signal = signal
-#         self._slot = slot
-#
-#     def update(self):
-#         self._slot(self._signal)

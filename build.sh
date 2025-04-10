bcfc19291266dd5faaae59aa659d445349a6a414
MJLMills <mjohnmills@gmail.com>
2025-04-09 22:31:03 -0500

    update to use CURL to grab micropython source code

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
-cd modules/micropython/ports/rp2
+curl -L https://github.com/micropython/micropython/releases/download/v1.24.1/micropython-1.24.1.tar.xz > micropython.tar.xz
+tar -xvf micropython.tar.xz
+
+cd micropython-1.24.1/ports/rp2
 make submodules
 
 cd ../
 make -C mpy-cross
 
-cd modules/micropython/ports/rp2
+cd micropython-1.24.1/ports/rp2
 make BOARD=RPI_PICO submodules
 make -j 8 BOARD=RPI_PICO FROZEN_MANIFEST=/Users/mjohnmills/PycharmProjects/mtmws_od/manifest.py
 

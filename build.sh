61f5be8e4ace24cfd0a088d8b18e747fd40c055c
MJLMills <mjohnmills@gmail.com>
2025-04-11 10:28:28 -0500

    add make clean step to build

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@ make -C mpy-cross
 
 cd ports/rp2
 make BOARD=RPI_PICO submodules
+make clean
 make -j 8 BOARD=RPI_PICO FROZEN_MANIFEST=/Users/mjohnmills/PycharmProjects/mtmws_od/manifest.py
-ls build-RPI_PICO/firmware.uf2
-mkdir ../../../dist
-cp -rf build-RPI_PICO/firmware.uf2 ../../../dist
 
-#cd ..
-#rm -rf micropython
+mkdir -p ../../../dist
+cp -rf build-RPI_PICO/firmware.uf2 ../../../dist

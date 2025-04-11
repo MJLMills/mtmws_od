abda217c4f9ec0d07fb5f1224271367ede35b4ae
MJLMills <mjohnmills@gmail.com>
2025-04-10 21:23:03 -0500

    minimize code to build uf2 for dist

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
-curl -L https://github.com/micropython/micropython/releases/download/v1.24.1/micropython-1.24.1.tar.xz > micropython.tar.xz
-tar -xvf micropython.tar.xz
-
-cd micropython-1.24.1/ports/rp2
-make submodules
-
-cd ../
+cd micropython
 make -C mpy-cross
 
-cd micropython-1.24.1/ports/rp2
+cd ports/rp2
 make BOARD=RPI_PICO submodules
 make -j 8 BOARD=RPI_PICO FROZEN_MANIFEST=/Users/mjohnmills/PycharmProjects/mtmws_od/manifest.py
-
 ls build-RPI_PICO/firmware.uf2
+mkdir ../../../dist
+cp build-RPI_PICO/firmware.uf2 ../../../dist
+
+#cd ..
+#rm -rf micropython
